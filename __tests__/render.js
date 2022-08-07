2b344776c6bd0a492aca3eb2c4d2fe3daaf4317c
m-guidali <57319159+m-guidali@users.noreply.github.com>
2022-08-07 11:53:29 +0200

    Remove unnnecessary plugins

--- __tests__/render.js
+++ __tests__/render.js
@@ -1,31 +1,9 @@
 import MarkdownIt from 'markdown-it';
-import MarkdownItAbbr from 'markdown-it-abbr';
-import MarkdownItAnchor from 'markdown-it-anchor';
-import MarkdownItDeflist from 'markdown-it-deflist';
 import MarkdownItEmoji from 'markdown-it-emoji';
-import MarkdownItFootnote from 'markdown-it-footnote';
-import MarkdownItHighlightjs from 'markdown-it-highlightjs';
-import MarkdownItIns from 'markdown-it-ins';
-import MarkdownItMark from 'markdown-it-mark';
-import MarkdownItSub from 'markdown-it-sub';
-import MarkdownItSup from 'markdown-it-sup';
-import MarkdownItTasklists from 'markdown-it-task-lists';
-import MarkdownItTOC from 'markdown-it-toc-done-right';
 
 const md = new MarkdownIt();
 const render = (source) => md.render(source);
 
-md.use(MarkdownItAbbr)
-  .use(MarkdownItAnchor)
-  .use(MarkdownItDeflist)
-  .use(MarkdownItEmoji)
-  .use(MarkdownItFootnote)
-  .use(MarkdownItHighlightjs)
-  .use(MarkdownItIns)
-  .use(MarkdownItMark)
-  .use(MarkdownItSub)
-  .use(MarkdownItSup)
-  .use(MarkdownItTasklists)
-  .use(MarkdownItTOC);
+md.use(MarkdownItEmoji);
 
 export default render;
